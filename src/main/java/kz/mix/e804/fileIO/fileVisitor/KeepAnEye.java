4f97c6f8fedff171fbc26dc7d430fa046027b630
Михаил Подольский <podolskiy.mikhail@inessoft.kz>
2016-10-04 15:19:27 +0600

    WatchService обновление

--- src/main/java/kz/mix/e804/fileIO/fileVisitor/KeepAnEye.java
+++ src/main/java/kz/mix/e804/fileIO/fileVisitor/KeepAnEye.java
@@ -11,11 +11,12 @@ import java.nio.file.WatchService;
 
 public class KeepAnEye {
     public static void main(String[] args) {
-        Path path = Paths.get("C:\\Users\\Podolskiy.Mikhail\\Desktop\\1\\12");
+        Path path = Paths.get("C:\\Users\\Podolskiy.Mikhail\\Desktop\\FILES");
         WatchService watchService = null;
         try {
             watchService = path.getFileSystem().newWatchService();
-            path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY);
+            path.register(watchService, StandardWatchEventKinds.ENTRY_MODIFY, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE,
+                    StandardWatchEventKinds.OVERFLOW);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -34,8 +35,14 @@ public class KeepAnEye {
                     case "OVERFLOW":
                         System.out.println("We lost some events");
                         break;
+                    case "ENTRY_CREATE":
+                        System.out.println("File " + event.context() + " is created!");
+                        break;
                     case "ENTRY_MODIFY":
                         System.out.println("File " + event.context() + " is changed!");
+                        break;
+                    case "ENTRY_DELETE":
+                        System.out.println("File " + event.context() + " is deleted!");
                         break;
                 }
             }
